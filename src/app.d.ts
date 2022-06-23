9b24e2e12f59ba1fe0ad6eb993b461c229b93ebc
Kittiphum-Kraisit <63011175@kmitl.ac.th>
2022-06-23 15:05:50 +0700

    edit app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,18 @@
 // for information about these interfaces
 declare namespace App {
 	// interface Locals {}
-	// interface Platform {}
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		}
+	}
 	// interface Session {}
 	// interface Stuff {}
 }
+
+export async function post({ request, platform }) {
+	const counter = platform.env.COUNTER.idFromName('A');
+  }
